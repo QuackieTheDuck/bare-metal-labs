06844dadcc38fa944725eaea7846ae98e3fdfca9
QuackieTheDuck <drozdekrobert2@gmail.com>
2026-07-28 16:48:50 +0200

    finished ADC cnfiguration

--- STM32/007_UART_&_ADC/ADC_Module/ADC_cnfg.c
+++ STM32/007_UART_&_ADC/ADC_Module/ADC_cnfg.c
@@ -18,13 +18,24 @@ void ADC_setup (){
     ADC->CFGR1 &= ~(HRM_ADC_CFGR1_DMACFG);
 
     // ADC_SMPR since the analog input will be simulated by potentiometer it is set to 111
+    ADC->SMPR |= (HRM_ADC_SMPR_SMP_0 | HRM_ADC_SMPR_SMP_1 | HRM_ADC_SMPR_SMP_2);
 
     // ADC_CHSELR
+    ADC->CHSELR |= HRM_ADC_CHSELR_0;
 
     // ADC_CCR
+    ADC->CCR &= ~(HRM_ADC_CCR_VREFEN);
 
-    // Then do the calibration
+    // ADC calibration
+    ADC->CR |= HRM_ADC_CR_ADCAL;
+    while (( ADC->CR & HRM_ADC_CR_ADCAL) != 0){
+        __NOP();
+    }
 
-    // ADC enable after all configuration processes are done
+    // ADC enable
+    ADC->ISR |= HRM_ADC_ISR_ADRDY;
     ADC->CR |= HRM_ADC_CR_ADEN;
+    while ((ADC->ISR & HRM_ADC_ISR_ADRDY) == 0){
+        __NOP();
+    }
 }
---
+++ STM32/007_UART_&_ADC/DMA_Module/DMA_cnfg.h
@@ -0,0 +1,8 @@
+#ifndef DMA_CNFG_H_
+#define DMA_CNFG_H_
+
+#include <HRM.h>
+
+void DMA_setup ();
+
+#endif /* DMA_CNFG_H_ */
